091848a4ef1b49e3376ad4183f24d8c4e8d207d3
ikyoungtae <go66go66@naver.com>
2025-07-10 10:51:21 +0900

    init

--- src/main/java/com/login/LoginController.java
+++ src/main/java/com/login/LoginController.java
@@ -1,10 +1,8 @@
 package com.login;
 
-import com.system.auth.authuser.AuthUser;
-import com.system.auth.domain.Token;
+import com.system.auth.authuser.AuthUserService;
 import com.system.auth.dto.AuthenticationRequest;
 import com.system.auth.dto.AuthenticationResponse;
-import com.system.auth.authuser.AuthUserService;
 import com.system.auth.jwt.JwtService;
 import com.system.auth.service.AuthenticationService;
 import com.system.auth.token.TokenService;
@@ -16,10 +14,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,10 +31,6 @@ import java.util.*;
 public class LoginController {
     private final AuthenticationService authenticationService;
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
-    private final AuthUserService authUserService;
-    private final TokenService tokenService;
-    private final PasswordEncoder passwordEncoder;
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @GetMapping("")
@@ -55,8 +48,8 @@ public class LoginController {
             String userEmail;
             userEmail = jwtService.extractUsername(jwt);
             if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() != null) {
-                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-                if (jwtService.isTokenValid(jwt, userDetails)) {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication != null && authentication.isAuthenticated() && !(authentication instanceof AnonymousAuthenticationToken)) {
                     return "layout/home";
                 }else{
                     return "redirect:/api/v1/auth/logout";
